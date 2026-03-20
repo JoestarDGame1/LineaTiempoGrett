a782c2ac1d426822f6a39698fd152f07070f12bb
JoestarDGame1 <jimenezvazquezjared60@gmail.com>
2026-03-20 16:54:38 -0600

    Actualización de diseño

--- data/preguntas.js
+++ data/preguntas.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const preguntas = [
   {
     pregunta: "¿Dónde te vi por primera vez?",
@@ -51,58 +50,4 @@ const preguntas = [
     opciones: ["Pol", "Romi", "Rose", "Rosy"],
     correcta: 0
   }
-=======
-const preguntas = [
-  {
-    pregunta: "¿Dónde te vi por primera vez?",
-    opciones: ["Escuela", "Desfile", "Facebook", "Concurso"],
-    correcta: 3
-  },
-  {
-    pregunta: "¿Donde salimos por primera vez?",
-    opciones: ["Café", "Paseo del Rio", "Alameda", "Parque Lopez"],
-    correcta: 2
-  },
-  {
-    pregunta: "¿Qué me gusta más de ti?",
-    opciones: ["Tu sonrisa", "Tu forma de ser", "Todo de ti", "Tus ojos"],
-    correcta: 2
-  },
-  {
-    pregunta: "¿Donde fue nuestro primer beso?",
-    opciones: ["Cine", "Café", "Parque", "Otro lugar"],
-    correcta: 3
-  },
-  {
-    pregunta: "¿Cuantos peluche te he dado?",
-    opciones: ["6", "4", "5", "7"],
-    correcta: 2
-  },
-
-  {
-    pregunta: "¿Qué pelicula no hemos visto juntos?",
-    opciones: ["Yo Antes de ti", "Shrek", "La momia", "Todas las anteriores"],
-    correcta: 2
-  },
-  {
-    pregunta: "¿Cual fue la primera pelicula que vimos juntos?",
-    opciones: ["Yo Antes de ti", "Shrek", "La monja", "Todas las anteriores"],
-    correcta: 2
-  },
-  {
-    pregunta: "¿Cual fue la primera flor que te regale?",
-    opciones: ["Gerbera", "Rosa", "Girasol", "Orquídea"],
-    correcta: 2
-  },
-  {
-    pregunta: "Fecha de nuestra primera salida",
-    opciones: ["20-09-25", "20-07-25", "30-07-25", "07-08-25"],
-    correcta: 2
-  },
-  {
-    pregunta: "¿Quien de ellos NO te regale?",
-    opciones: ["Pol", "Romi", "Rose", "Rosy"],
-    correcta: 0
-  }
->>>>>>> 6927f25f480e3e1fac3ecbfb849bd618dad22c0f
 ];
