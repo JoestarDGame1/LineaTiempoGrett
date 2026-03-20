0204d03dfac960a42bdf6d823c1ea49ff1278c8e
JoestarDGame1 <jimenezvazquezjared60@gmail.com>
2026-03-20 16:04:22 -0600

    Update timeline.js

--- js/timeline.js
+++ js/timeline.js
@@ -1,68 +1,39 @@
-document.addEventListener("DOMContentLoaded", () => {
+const container = document.getElementById("timeline");
 
-  const container = document.getElementById("timeline");
-  let imagenes = [];
+// CREAR EVENTOS
+momentos.forEach((m, i) => {
+  const div = document.createElement("div");
 
-  momentos.forEach((m, i) => {
-    const div = document.createElement("div");
-    div.classList.add("evento");
+  div.classList.add("evento");
 
-    if (i % 2 === 0) {
-      div.classList.add("left");
-    } else {
-      div.classList.add("right");
-    }
+  if (i % 2 === 0) {
+    div.classList.add("left");
+  } else {
+    div.classList.add("right");
+  }
 
-    const img = new Image();
-    img.src = m.imagen;
-    imagenes.push(img);
+  div.innerHTML = `
+    <div class="contenido">
+      <h3>${m.fecha}</h3>
+      <p>${m.texto}</p>
+      <img src="${m.imagen}">
+    </div>
+  `;
 
-    div.innerHTML = `
-      <div class="contenido">
-        <h3>${m.fecha}</h3>
-        <p>${m.texto}</p>
-        <img src="${m.imagen}">
-      </div>
-    `;
-
-    container.appendChild(div);
-  });
-
-  let cargadas = 0;
-  let total = imagenes.length;
-
-  // seguridad: máximo espera 2 segundos
-  setTimeout(() => {
-    document.querySelector(".timeline").style.opacity = "1";
-    document.body.style.opacity = "1";
-  }, 2000);
-
-  imagenes.forEach(img => {
-    img.onload = img.onerror = () => {
-      cargadas++;
-
-      if (cargadas === total) {
-        document.querySelector(".timeline").style.opacity = "1";
-        document.body.style.opacity = "1";
-      }
-    };
-  });
-
-    // 🔥 ACTIVAR ANIMACIONES AL HACER SCROLL
-  const eventos = document.querySelectorAll(".evento");
-
-  const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("show");
-      }
-    });
-  }, {
-    threshold: 0.2
-  });
-
-  eventos.forEach(evento => {
-    observer.observe(evento);
-  });
-  
+  container.appendChild(div);
 });
+
+// ANIMACIÓN AL SCROLL
+const elementos = document.querySelectorAll(".evento, .final");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+elementos.forEach(el => observer.observe(el));
