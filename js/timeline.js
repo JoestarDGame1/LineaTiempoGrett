8e229e69e2ed5e3ead325b0fccae3075ba37572a
JoestarDGame1 <jimenezvazquezjared60@gmail.com>
2026-03-20 15:32:52 -0600

    Update timeline.js

--- js/timeline.js
+++ js/timeline.js
@@ -1,52 +1,51 @@
-const container = document.getElementById("timeline");
+document.addEventListener("DOMContentLoaded", () => {
 
-let imagenes = [];
+  const container = document.getElementById("timeline");
+  let imagenes = [];
 
-momentos.forEach((m, i) => {
-  const div = document.createElement("div");
-  div.classList.add("evento");
+  momentos.forEach((m, i) => {
+    const div = document.createElement("div");
+    div.classList.add("evento");
 
-  if (i % 2 === 0) {
-    div.classList.add("left");
-  } else {
-    div.classList.add("right");
-  }
-
-  const img = new Image();
-  img.src = m.imagen;
-  imagenes.push(img);
-
-  div.innerHTML = `
-    <div class="contenido">
-      <h3>${m.fecha}</h3>
-      <p>${m.texto}</p>
-      <img src="${m.imagen}">
-    </div>
-  `;
-
-  container.appendChild(div);
-});
-
-let cargadas = 0;
-let total = imagenes.length;
-
-// 🔥 seguridad: máximo espera 2 segundos
-setTimeout(() => {
-  document.querySelector(".timeline").style.opacity = "1";
-  document.body.style.opacity = "1";
-}, 2000);
-
-imagenes.forEach(img => {
-  img.onload = img.onerror = () => {
-    cargadas++;
-
-    if (cargadas === total) {
-      document.querySelector(".timeline").style.opacity = "1";
-      document.body.style.opacity = "1";
+    if (i % 2 === 0) {
+      div.classList.add("left");
+    } else {
+      div.classList.add("right");
     }
-  };
-});
 
-window.onload = () => {
-  document.body.style.opacity = "1";
-};
+    const img = new Image();
+    img.src = m.imagen;
+    imagenes.push(img);
+
+    div.innerHTML = `
+      <div class="contenido">
+        <h3>${m.fecha}</h3>
+        <p>${m.texto}</p>
+        <img src="${m.imagen}">
+      </div>
+    `;
+
+    container.appendChild(div);
+  });
+
+  let cargadas = 0;
+  let total = imagenes.length;
+
+  // seguridad: máximo espera 2 segundos
+  setTimeout(() => {
+    document.querySelector(".timeline").style.opacity = "1";
+    document.body.style.opacity = "1";
+  }, 2000);
+
+  imagenes.forEach(img => {
+    img.onload = img.onerror = () => {
+      cargadas++;
+
+      if (cargadas === total) {
+        document.querySelector(".timeline").style.opacity = "1";
+        document.body.style.opacity = "1";
+      }
+    };
+  });
+
+});
